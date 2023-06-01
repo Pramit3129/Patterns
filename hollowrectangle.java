83ad5831c1ff0aaebd1be3a6f4d5262addbe103f
Pramit <87760379+Pramit3129@users.noreply.github.com>
2023-06-01 09:58:23 +0530

    Add files via upload

---
+++ hollowrectangle.java
@@ -0,0 +1,15 @@
+public class hollowrectangle{
+    public static void main(String[] args){
+        for(int i=1;i<=6;i++){
+            for (int j=1; j<=7;j++){
+                if(i==1 || j==1 || i==6 || j==7){
+                    System.out.print("*");}
+            else{
+                    System.out.print(" ");}
+            }
+                System.out.println(" ");
+                }
+            }
+        }
+
+
