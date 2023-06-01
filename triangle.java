674198f362c3bd2f0acc628a7581bb397066cf5f
Pramit <87760379+Pramit3129@users.noreply.github.com>
2023-06-01 10:00:36 +0530

    Add files via upload

---
+++ triangle.java
@@ -0,0 +1,9 @@
+public class triangle {
+    public static void main(String[] args){
+        for(int i=4; i>=1; i=i-1){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");}
+            System.out.println();}
+            }
+        }
+
